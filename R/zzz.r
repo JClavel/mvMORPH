042892189beb50110f3336947f60199c53c0ffec
Clavel Julien <julien.clavel@hotmail.fr>
2016-06-08 12:10:51 +0200

    citation

--- R/zzz.r
+++ R/zzz.r
@@ -4,7 +4,6 @@
     packageStartupMessage("##\n## mvMORPH package (1.0.7)")
     packageStartupMessage("## Multivariate evolutionary models")
     packageStartupMessage("##\n## See the tutorials: browseVignettes(\"mvMORPH\")")
-    packageStartupMessage("##\n## Clavel et al. (2015)")
-    packageStartupMessage("## (Methods in Ecology and Evolution. 6(11):1311-1319)\n##")
+    packageStartupMessage("##\n## To cite package 'mvMORPH': citation(\"mvMORPH\")\n##")
 
 }
