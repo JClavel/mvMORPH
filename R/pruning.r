4aa1b402157486265501dd51df0e0b6711655038
Clavel Julien <julien.clavel@hotmail.fr>
2017-08-11 23:00:44 +0200

    check tests
    add an option to ask for checks. Maybe useful in recursive algorithms.
    Return the transpose of the square root. Save times by avoiding to
    compute the transpose and allows the use of faster « crossprod »
    function

--- R/pruning.r
+++ R/pruning.r
@@ -2,10 +2,13 @@
 # See also Stone 2011 - Syst. Bio. and Khabazzian et al. 2016 - Meth. Ecol. Evol.; for computing the matrix square root from the prunning algorithm
 # inv = TRUE compute the square root of the inverse covariance matrix, inv=FALSE compute the square root of the covariance matrix
 
-pruning <- function(tree, inv=TRUE){
+pruning <- function(tree, inv=TRUE, check=TRUE){
   # check the order of the tree; prunning algorithm use "postorder"
-  if(!is.binary.tree(tree)) tree <- multi2di(tree, random=FALSE)
-  if(attr(tree,"order")!="postorder") tree <- reorder.phylo(tree, "postorder")
+  if(check==TRUE){
+      if(!is.binary.tree(tree)) tree <- multi2di(tree, random=FALSE)
+      if(attr(tree,"order")!="postorder") tree <- reorder.phylo(tree, "postorder")
+  }
+  
   invMat=1*inv
   mode(invMat)="integer"
   
@@ -14,7 +17,7 @@ pruning <- function(tree, inv=TRUE){
   
   logdet <- sum(log(c(prunRes[[2]], prunRes[[3]]))) # log-determinant
   
-  results <- list(sqrtMat=t(prunRes[[1]]), varNode=prunRes[[2]], varRoot=prunRes[[3]], det=logdet)
+  results <- list(sqrtMat=prunRes[[1]], varNode=prunRes[[2]], varRoot=prunRes[[3]], det=logdet)
   # we can also just rename the list slots (names(prunRes) = c("sqrtMat","varNode","varRoot","det"))
   class(results) <- c("mvmorph.var")
   return(results)
